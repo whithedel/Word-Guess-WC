fe559bbe1c5c00732fe7dc01a5888a6737f06527
Whithedel Carre <Whithedel@gmail.com>
2019-05-04 01:02:47 -0400

    updates js file

--- Archive/logic.js
+++ Archive/logic.js
@@ -19,8 +19,16 @@ function isCorrectGuess(Word, letter) {
         return true;
     }
     return false;
-};
+}
+// return new RegExp(letter, "gi").test(word)
 
+function otherCorrectGuess(word, letter){
+    // for(var i = 0; i < word.length; i++){
+        if (word.match(letter)){
+            return true
+        }return false
+    }
+// }
 
 
 //function for blanks of the word
@@ -37,13 +45,50 @@ function getBlanks(word) {
 //funtion to fill in the blank with the guessed letter
 
 function fillBlanks(randomWord, currentPuzzleState, letter) {
-
     
-  var letter = false;
-  for (var i = 0; i < currentPuzzleState; i++) {
-    if (randomWord[i] === letter) {
-       letter = true;
+    for ( var i=0; i < randomWord.length; i+=1) {
+        if (randomWord[i] === letter) {
+            currentPuzzleState[i] = letter;
+        }
     }
-  }
+    return currentPuzzleState;
 }
-  
+
+
+// creating the game round object.
+
+function setupRound(theWord) {
+    var roundObject = {
+        word:theWord,
+        guessesLeft:9,
+        wrongGuesses:[],
+        puzzleState:getBlanks(theWord),
+    }
+    return roundObject;
+}
+
+//updating the game base on correct guessed
+
+function updateRound(setupRound,letter) {
+    if (isCorrectGuess(setupRound.word,letter)) {
+        setupRound.puzzleState = fillBlanks(setupRound.word,setupRound.puzzleState,letter);
+
+    }
+    else {
+        setupRound.guessesLeft -=1;
+        setupRound.wrongGuesses.push(letter);
+    }
+}
+
+//check to see if user has won the round or not 
+
+function hasWon(puzzleState) {
+    setupRound.puzzleState=puzzleState;
+    if (puzzleState.toString().match('_')) {
+        return false }
+        else {
+            return true
+        }
+    }
+
+
